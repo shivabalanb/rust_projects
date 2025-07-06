cb5a3dc3d36e4b5947fc8a5c9018f7a67fabccf8
Shiva <ShivaBala2015@gmail.com>
2025-07-06 13:56:02 -0400

    07/06

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,12 +1,25 @@
-fn main() {
-    let mut counter = 0;
+use std::vec;
 
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter;
-        }
-    };
+fn main() {
+
+    let mut v = vec![1,2,3];
+    let num = v[2];
+    v.push(2);
+    println!("{}",num);
+
+    // let m1 = String::from("Hello");
+    // let m2 = String::from("world");
+    // let (m1, m2) = greet(m1, m2);
+    // let s = format!("{} {}", m1, m2);
+
+    // let mut counter = 0;
+
+    // let result = loop {
+    //     counter += 1;
+    //     if counter == 10 {
+    //         break counter;
+    //     }
+    // };
 
     // let num=3;
 
@@ -17,6 +30,11 @@ fn main() {
     // }
 
     // another_function(5);
+}
+
+fn greet(g1: String, g2: String) -> (String, String) {
+    println!("{} {}!", g1, g2);
+    (g1, g2)
 }
 
 fn another_function(num: i32) {
