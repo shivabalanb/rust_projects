567d04d84aababb941cac4ad3f547f6368b02303
Shiva Balathandayuthapani <shiva.balathandayuth@gemini.com>
2025-07-11 17:36:36 -0400

    07/11 partial

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,11 @@
+struct User {
+    name: String,
+    has_access: bool
+}
+
 fn main() {
+    let u1 = User{
+        
+    }
     println!("Hello world!");
 }
--- treesap/target/debug/deps/treesap-89276b4d4de95fad.d
+++ treesap/target/debug/deps/treesap-89276b4d4de95fad.d
@@ -1,5 +1,5 @@
-/Users/shivabalathandayuthapani/Documents/Projects/rust_projects/treesap/target/debug/deps/treesap-89276b4d4de95fad.d: src/main.rs
+/Users/shiva.balathandayuthapani/Documents/rust_projects/treesap/target/debug/deps/treesap-89276b4d4de95fad.d: src/main.rs
 
-/Users/shivabalathandayuthapani/Documents/Projects/rust_projects/treesap/target/debug/deps/libtreesap-89276b4d4de95fad.rmeta: src/main.rs
+/Users/shiva.balathandayuthapani/Documents/rust_projects/treesap/target/debug/deps/libtreesap-89276b4d4de95fad.rmeta: src/main.rs
 
 src/main.rs:
