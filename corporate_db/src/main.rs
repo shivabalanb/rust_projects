6e519bf141e5da41ceb3156f167905a85214a974
Shiva <ShivaBala2015@gmail.com>
2025-07-20 17:21:52 -0400

    07/20

--- corporate_db/src/main.rs
+++ corporate_db/src/main.rs
@@ -3,35 +3,87 @@ use std::{collections::HashMap, io};
 // DEPARTMENT -> USER[]
 // output DEPARTMENT -> sorted USERs
 // sorted USERs -> DEPARTMENT
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
+struct User(String);
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct Department(String);
+
 fn main() {
-    let mut db: HashMap<String, Vec<String>> = HashMap::new();
+    let mut db: HashMap<Department, Vec<User>> = HashMap::new();
     println!("Welcome to the corporate db!");
 
+    loop {
+        println!("\n\n");
+        println!("Press 1 to add user to a department");
+        println!("Press 2 to view db");
+        println!("Press -1 to exit");
+        let mut option = String::new();
+
+        io::stdin().read_line(&mut option).expect("failed");
+
+        let option: i32 = match option.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match option {
+            -1 => break,
+            1 => add_user_to_department(&mut db),
+            2 => view_users_in_department(&mut db),
+            _ => continue,
+        }
+    }
+
+    println!("{:?}", db);
+}
+
+fn add_user_to_department(db: &mut HashMap<Department, Vec<User>>) {
     println!("Which user would you like to add?");
     let mut user = String::new();
 
     io::stdin().read_line(&mut user).expect("failed");
 
-    let user: String = match user.trim().parse() {
-        Ok(user_str) => user_str,
-        Err(_) => return,
-    };
+    let user: User = User(user.trim().to_string());
 
-    println!("What department would you like to add {user} to?");
+    println!("What department would you like to add {:?} to?", user);
     let mut department = String::new();
 
     io::stdin().read_line(&mut department).expect("failed");
 
-    let department: String = match department.trim().parse() {
-        Ok(department_str) => department_str,
-        Err(_) => return,
-    };
+    let department: Department = Department(department.trim().to_string());
+    let department_name = department.0.clone();
 
     let users = db.entry(department).or_insert(Vec::new());
 
-    if !users.contains(&user) {
-        users.push(user);
+    if users.contains(&user) {
+        println!(
+            "User '{:?}' already exists in department '{:?}'. Not adding.",
+            user, department_name
+        );
+        return;
     }
 
-    println!("{:?}", db);
+    let insert_idx = match users.binary_search(&user) {
+        Ok(idx) => idx,
+        Err(idx) => idx,
+    };
+
+    users.insert(insert_idx, user);
+}
+
+fn view_users_in_department(db: &mut HashMap<Department, Vec<User>>) {
+    println!("What department would you like to view?");
+    let mut department = String::new();
+
+    io::stdin().read_line(&mut department).expect("failed");
+
+    let department: Department = Department(department.trim().to_string());
+
+    match db.get(&department) {
+        Some(users) => println!(
+            "Department {:?} has the following users: {:?}",
+            &department, &users
+        ),
+        None => println!("No users in {:?}", &department),
+    };
 }
--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,24 +1,12 @@
+fn main() {
+    let v = vec![1, 2, 3];
+
+    v[99];
+}
+
 struct User {
     name: String,
     has_access: bool,
 }
 
 struct Color(i32, i32, i32);
-
-fn main() {
-    let mut u1: User = User {
-        name: String::from("shiv"),
-        has_access: false,
-    };
-    let u2: User = User {
-        name: u1.name,
-        has_access: u1.has_access,
-    };
-
-    u1.name = String::from("hey");
-
-    let c1 = Color(0,0,0);
-
-    println!("hey {}", u1.name);
-    println!("hey {}", u2.name);
-}
